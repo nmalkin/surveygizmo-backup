74afb538c9a55ddec5490321031e65bc4dbe096b
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-08-20 13:01:24 -0700

    Update useragent

--- download_survey.py
+++ download_survey.py
@@ -19,7 +19,7 @@ TARGET_DIRECTORY = 'surveygizmo'
 
 HEADERS = {
     'Accept-Language': 'en-US,en;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 }
 
 
